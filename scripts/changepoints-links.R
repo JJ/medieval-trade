7534acd6d25f3a78f482c2d51ec7d1768dd98f40
JJ Merelo <jjmerelo@gmail.com>
2024-12-31 09:25:55 +0100

    #4 :sparkles: data file

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -4,4 +4,6 @@ library(ggplot2)
 links <- read.csv("data/links.csv",sep=";")
 
 links %>% group_by(year) %>%
-  summarise(n = n())
+  summarise(n = n()) -> links_year
+
+saveRDS(links_year, "data/links_year.rds")
