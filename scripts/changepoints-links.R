0765b75172bdb955391a85c61d7eb9a817143962
JJ Merelo <jjmerelo@gmail.com>
2024-12-31 08:55:08 +0100

    :recycle:

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -3,6 +3,5 @@ library(ggplot2)
 
 links <- read.csv("data/links.csv",sep=";")
 
-# count the number of rows for each year and store it in a new data frame
 links %>% group_by(year) %>%
   summarise(n = n())
