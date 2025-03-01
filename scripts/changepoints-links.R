8acb6cf32cc9b90aa003f039a03460ff0dba5e99
JJ Merelo <jjmerelo@gmail.com>
2025-03-01 19:48:16 +0100

    #9 :sparkles: generates dataset

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-links <- read.csv("data/iberian-links.csv",sep=";")
+links <- read.csv("data/all-iberian-links.csv",sep=";")
 links$decade <- floor( links$year / 10 ) * 10
 
 links %>% group_by(decade) %>%
@@ -12,5 +12,5 @@ for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
   }
 }
 
-saveRDS(links_decade, "data/iberian_links_decade.rds")
-saveRDS(links, "data/iberian_links.rds")
+saveRDS(links_decade, "data/all_iberian_links_decade.rds")
+saveRDS(links, "data/all_iberian_links.rds")
