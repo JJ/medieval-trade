01f53eb68366e387f8a86f8a3fe611f368e80586
JJ Merelo <jjmerelo@gmail.com>
2025-03-01 11:37:29 +0100

    :coffin:

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -1,9 +1,16 @@
 library(dplyr)
-library(ggplot2)
 
 links <- read.csv("data/iberian-links.csv",sep=";")
+links$decade <- floor( links$year / 10 ) * 10
 
-links %>% group_by(year) %>%
-  summarise(n = n()) -> links_year
+links %>% group_by(decade) %>%
+  summarise(n = n()) -> links_decade
 
-saveRDS(links_year, "data/iberian_links_year.rds")
+for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
+  if ( !decade %in% links_decade$decade ) {
+    links_decade <- rbind(links_decade, data.frame(decade=decade, n=0))
+  }
+}
+
+saveRDS(links_decade, "data/iberian_links_decade.rds")
+saveRDS(links, "data/iberian_links.rds")
