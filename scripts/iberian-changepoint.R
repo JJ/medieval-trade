6ac6ff377eb272f7692d5865f041c8795ae14bbc
JJ Merelo <jjmerelo@gmail.com>
2025-03-01 21:09:03 +0100

    Adapting data #9

--- scripts/iberian-changepoint.R
+++ scripts/iberian-changepoint.R
@@ -14,3 +14,16 @@ ticks.years <- links_decade$decade[ticks]
 
 plot(link.changepoint,xaxt = 'n')
 axis(1, at = ticks, labels=ticks.years)
+
+all_links_decade_raw <- readRDS("data/all_iberian_links_decade.rds")
+all_links_decade <- all_links_decade_raw[order(all_links_decade_raw$decade),]
+all_links_decade$links <- all_links_decade$n
+all_links_decade$n <- NULL
+
+all.link.changepoint <- cpt.mean(all_links_decade$links)
+all.link.changepoint.date <- all_links_decade$decade[all.link.changepoint@cpts]
+ticks <- c(1,10,20,30,40,50,60)
+ticks.years <- all_links_decade$decade[ticks]
+
+plot(all.link.changepoint,xaxt = 'n')
+axis(1, at = ticks, labels=ticks.years)
