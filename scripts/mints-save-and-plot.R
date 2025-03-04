b030ef9a7c31709f1cc928e6c7e0c94588e47751
JJ Merelo <jjmerelo@gmail.com>
2025-03-04 20:24:25 +0100

    #13 created data set for paper

--- scripts/mints-save-and-plot.R
+++ scripts/mints-save-and-plot.R
@@ -1,10 +1,12 @@
 library(ggplot2)
 
-mints.decade <- read.csv("data-raw/mints_decade.csv")
+mints.decade <- read.csv("data-raw/number-of-mints-decade.csv", sep=";")
 
-ggplot( mints.decade, aes(x=decade, y=mints)) +
+ggplot( mints.decade, aes(x=Decade, y=Mints)) + geom_point() + geom_smooth() +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(title = "Mints in Iberia by Decade",
        x = "Decade",
        y = "Number of Mints")
+
+saveRDS(mints.decade, "data/mints_decade.rds")
