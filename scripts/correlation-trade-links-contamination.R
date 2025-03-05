a3e79ac2978e116bf26e609fe737fda31131b6ee
JJ Merelo <jjmerelo@gmail.com>
2025-03-05 14:37:14 +0100

    #16 initial plot finding no correlation
    There are lots of high-pollution data points with very low activity

---
+++ scripts/correlation-trade-links-contamination.R
@@ -0,0 +1,12 @@
+
+pollution_data <- read.csv("data/pollution-data.csv", sep=";")
+iberian_links_decade <- readRDS("data/all_iberian_links_decade.rds")
+
+pollution_links <- merge(pollution_data, iberian_links_decade, by="decade")
+
+library(ggplot2)
+
+ggplot(pollution_links,aes(x=averagePg,y=n))+geom_point()
+
+model <- lm(n ~ averagePg, data=pollution_links)
+summary(model)
