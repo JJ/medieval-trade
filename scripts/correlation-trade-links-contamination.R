2baf378d330502bc0eedfbd323db4c4392e60ccf
JJ Merelo <jjmerelo@gmail.com>
2025-03-05 14:39:49 +0100

    No correlation or any kind here

--- scripts/correlation-trade-links-contamination.R
+++ scripts/correlation-trade-links-contamination.R
@@ -10,3 +10,10 @@ ggplot(pollution_links,aes(x=averagePg,y=n))+geom_point()
 
 model <- glm(n ~ averagePg, data=pollution_links)
 summary(model)
+
+iberian_links_decade <- readRDS("data/iberian_links_decade.rds")
+pollution_links <- merge(pollution_data, iberian_links_decade, by="decade")
+ggplot(pollution_links,aes(x=averagePg,y=n))+geom_point()
+
+model <- glm(n ~ averagePg, data=pollution_links)
+summary(model)
