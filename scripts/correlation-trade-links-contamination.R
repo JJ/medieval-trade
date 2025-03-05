54d9dd4ac8b3f23ba84154e1414c3616588ff317
JJ Merelo <jjmerelo@gmail.com>
2025-03-05 14:46:54 +0100

    No correlation with all data available #16

--- scripts/correlation-trade-links-contamination.R
+++ scripts/correlation-trade-links-contamination.R
@@ -1,19 +1,20 @@
 
 pollution_data <- read.csv("data/pollution-data.csv", sep=";")
-iberian_links_decade <- readRDS("data/all_iberian_links_decade.rds")
+links_decade <- readRDS("data/links_year.rds")
+links_decade$decade <- links_decade$year
 
-pollution_links <- merge(pollution_data, iberian_links_decade, by="decade")
+pollution_links <- merge(pollution_data, links_decade, by="decade")
 
 library(ggplot2)
 
 ggplot(pollution_links,aes(x=averagePg,y=n))+geom_point()
 
-model <- glm(n ~ averagePg, data=pollution_links)
+model <- lm(averagePg ~ n, data=pollution_links )
 summary(model)
 
 iberian_links_decade <- readRDS("data/iberian_links_decade.rds")
 pollution_links <- merge(pollution_data, iberian_links_decade, by="decade")
 ggplot(pollution_links,aes(x=averagePg,y=n))+geom_point()
 
-model <- glm(n ~ averagePg, data=pollution_links)
+model <- lm(averagePg ~ n, data=pollution_links)
 summary(model)
