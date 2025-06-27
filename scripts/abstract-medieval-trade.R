b4d57106f4af716f02658246d8e4554439ee30a4
JJ Merelo <jjmerelo@gmail.com>
2025-06-27 13:31:52 +0200

    Final form

--- scripts/abstract-medieval-trade.R
+++ scripts/abstract-medieval-trade.R
@@ -20,8 +20,15 @@ ticks <- c(1,100,200,300,400,500,600)
 ticks.years <- links_year$year[ticks]
 
 # set y label to "# links"
-
-plot(link.changepoint,xaxt = 'n', ylab="# Links", main="Number of links per year and mean before/after changepoint")
-axis(1, at = ticks, labels=ticks.years)
+png("statphys-changepoint.png",width=2000, height=1500, units="px")
+par(mar = c(5.1, 8.1, 4.1, 2.1))
+plot(link.changepoint,yaxt='n',xaxt = 'n', xlab="", ylab="", main="Number of links per year and mean before/after changepoint", cex=3, cex.main=3, cex.lab=4, cex.axis=3, cex.sub=3)
+# move position of tick labels down so that it does not overlaps ticks
+offset <- -0.05
+axis(1, at = ticks, labels=ticks.years, cex=3, cex.axis=3)
+axis(2, cex=3,cex.axis=3)
+mtext("# Links", side=2, line=5, cex=3)
+mtext("Year", side=1, line=4, cex=3)
+dev.off()
 
 
