1a4496c02a645b765b5161ba64b10a74915a7beb
JJ Merelo <jjmerelo@gmail.com>
2025-06-27 07:31:09 +0200

    replotting for poster

--- scripts/abstract-medieval-trade.R
+++ scripts/abstract-medieval-trade.R
@@ -1,15 +1,13 @@
-## ----statphys.abs, echo=FALSE, message=F, fig.height=4, fig.pos="h"-------------------------------------------------------------------------------
+
 links_year <- readRDS("data/links_year.rds")
 links_year$links <- links_year$n
 links_year$n <- NULL
-# fill with 0s any value of year that is missing
+
 library(dplyr)
 library(tidyr)
 
 links_year <- links_year %>%
   complete(year = full_seq(year, 1), fill = list(links = 0))
-
-# limit the data frame to the rows bwtween year 300 and 775
 
 links_year <- links_year %>%
   filter(year >= 300 & year <= 775)
@@ -21,7 +19,9 @@ link.changepoint.date <- links_year$year[link.changepoint@cpts]
 ticks <- c(1,100,200,300,400,500,600)
 ticks.years <- links_year$year[ticks]
 
-plot(link.changepoint,xaxt = 'n')
+# set y label to "# links"
+
+plot(link.changepoint,xaxt = 'n', ylab="# Links", main="Number of links per year and mean before/after changepoint")
 axis(1, at = ticks, labels=ticks.years)
 
 
