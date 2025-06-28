0156a2a0d989852afab554b61e0e18b33e37249f
JJ Merelo <jjmerelo@gmail.com>
2025-06-28 12:20:07 +0200

    Adds charts and reasons of decline

--- scripts/lead-pollution-plot-and-save.R
+++ scripts/lead-pollution-plot-and-save.R
@@ -1,6 +1,10 @@
 library(ggplot2)
+library(ggthemes)
 
 lead.pollution <- read.csv("data/pollution-data.csv",sep=";")
-ggplot(lead.pollution, aes(x=decade, y=averagePg)) + geom_point() + geom_smooth()
+ggplot(lead.pollution, aes(x=decade, y=averagePg)) + geom_point() + geom_smooth() + theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# Save the plot
+ggsave("lead_pollution_decade.png", width = 6, height = 10)
 
 saveRDS(lead.pollution, "data/lead_pollution_decade.rds")
