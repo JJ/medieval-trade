2895885de9c839c2dd2c44ef758f9c9e3d671fe6
JJ Merelo <jjmerelo@gmail.com>
2025-07-12 08:18:52 +0200

    #31 regenerating series

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -13,7 +13,6 @@ for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
 }
 
 saveRDS(links_decade, "data/all_iberian_links_decade.rds")
-saveRDS(links, "data/all_iberian_links.rds")
 
 links <- read.csv("data/iberian-links.csv",sep=";")
 links$decade <- floor( links$year / 10 ) * 10
@@ -26,5 +25,4 @@ for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
   }
 }
 
-saveRDS(links_decade, "data/iberian_links_decade.rds")
 saveRDS(links, "data/iberian_links.rds")
