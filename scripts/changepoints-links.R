e3e8a5cede74a57f14c4544057f13929d9b07ba2
JJ Merelo <jjmerelo@gmail.com>
2025-07-12 08:28:33 +0200

    #31 regenerating series with sorted order

--- scripts/changepoints-links.R
+++ scripts/changepoints-links.R
@@ -3,6 +3,8 @@ library(dplyr)
 links <- read.csv("data/all-iberian-links.csv",sep=";")
 links$decade <- floor( links$year / 10 ) * 10
 
+saveRDS(links, "data/all_iberian_links.rds")
+
 links %>% group_by(decade) %>%
   summarise(n = n()) -> links_decade
 
@@ -12,10 +14,13 @@ for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
   }
 }
 
+# sort by decade
+links_decade <- links_decade[order(links_decade$decade),]
 saveRDS(links_decade, "data/all_iberian_links_decade.rds")
 
 links <- read.csv("data/iberian-links.csv",sep=";")
 links$decade <- floor( links$year / 10 ) * 10
+saveRDS(links, "data/iberian_links.rds")
 
 links %>% group_by(decade) %>% summarise(n = n()) -> links_decade
 
@@ -25,4 +30,5 @@ for (decade in seq(min(links_decade$decade), max(links_decade$decade), 10)) {
   }
 }
 
-saveRDS(links, "data/iberian_links.rds")
+links_decade <- links_decade[order(links_decade$decade),]
+saveRDS(links_decade, "data/iberian_links_decade.rds")
