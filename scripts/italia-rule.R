215e38b8f3bed562c81518f0a626fb3027f3e826
JJ Merelo <jjmerelo@gmail.com>
2026-07-25 10:07:33 +0200

    Analyzing changepoints

--- scripts/italia-rule.R
+++ scripts/italia-rule.R
@@ -1,12 +1,26 @@
 library(dplyr)
+
 italia_links <- readRDS("data/flame-time-series-April-2026-filtered.rds") %>% filter( !is.na(link_rate) & (hoard == "Italia" | mint=="Italia") )
 
-Italia_intra_links <- italia_links %>% filter(hoard == "Italia" & mint=="Italia")
+italia_intra_links <- italia_links %>% filter(hoard == "Italia" & mint=="Italia")
 
-italia_links %>% mutate( year_group = floor( year / 5 ) * 5 ) %>% group_by( year_group ) %>% arrange( year_group ) %>% summarise( total_link_rate = sum( link_rate )) -> italia_links_year
-Italia_intra_links %>% mutate( year_group = floor( year / 5 ) * 5 ) %>% group_by( year_group ) %>% arrange( year_group ) %>% summarise( total_link_rate = sum( link_rate )) -> Italia_intra_links_year
+italia_links %>% group_by( year ) %>% arrange( year ) %>% summarise( link_density = sum( link_rate )) -> italia_links_year
+italia_intra_links %>% group_by( year ) %>% arrange( year ) %>% summarise( link_density = sum( link_rate )) -> italia_intra_links_year
 
 library(ggplot2)
 
-ggplot( italia_links_year, aes(x=year_group,y=total_link_rate,color="Total"))+ geom_line()+
-  geom_line(data=Italia_intra_links_year,aes(y=total_link_rate,color="Intra"))+theme_minimal()
+ggplot( italia_links_year, aes(x=year,y=link_density,color="Total"))+ geom_line()+
+  geom_line(data=italia_intra_links_year,aes(y=link_density,color="Intra"))+theme_minimal()
+
+italia_extra_links <- italia_links %>% filter(!(hoard == "Italia" & mint=="Italia"))
+italia_extra_links%>% group_by( year ) %>% arrange( year ) %>% summarise( link_density = sum( link_rate )) -> italia_extra_links_year
+
+library(ecp)
+z_links <- matrix(c(italia_extra_links_year$link_density, Italia_intra_links_year$link_density), ncol=2)
+
+multi_changepoint <- e.divisive(z_links, min.size=100)
+multi_changepoint_year <- italia_extra_links_year$year[multi_changepoint$estimate[2]]
+
+multi_changepoint_50y <- e.divisive(z_links, min.size=50)
+multi_changepoint_year_50y <- italia_extra_links_year$year[multi_changepoint_50y$estimate[2]]
+
