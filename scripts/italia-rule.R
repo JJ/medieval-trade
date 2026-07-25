77a7f7bdb3e87de1383a282838a0be93e7306ab4
JJ Merelo <jjmerelo@gmail.com>
2026-07-25 10:45:05 +0200

    Add chart post-cp

--- scripts/italia-rule.R
+++ scripts/italia-rule.R
@@ -56,7 +56,7 @@ ggraph(italia_pre_cp_graph, layout = 'stress') +
 
   # 1. Map edge weights to line thickness
   geom_edge_link(aes(linewidth = weight), color = "gray60", alpha = 0.6) +
-  scale_edge_width(range = c(0.5, 3), name = "Link Rate") +
+ # scale_edge_width(range = c(0.5, 3), name = "Link Rate") +
 
   # 2. Style the nodes
   geom_node_point(size = 7, fill = "#E69F00", color = "black", shape = 21) +
@@ -72,3 +72,33 @@ ggraph(italia_pre_cp_graph, layout = 'stress') +
   # 4. Clean up the background
   theme_graph() +
   theme(legend.position = "bottom")
+
+post_cp_data <- italia_links_combined %>%
+  filter( year >= multi_changepoint_year) %>%
+  summarise( .by=c(from,to), weight = sum(link_rate))
+
+italia_post_cp_graph <- graph_from_data_frame( post_cp_data,
+                                              directed=F )
+
+
+ggraph(italia_post_cp_graph, layout = 'stress') +
+
+  # 1. Map edge weights to line thickness
+  geom_edge_link(aes(linewidth = weight), color = "gray60", alpha = 0.6) +
+  # scale_edge_width(range = c(0.5, 3), name = "Link Rate") +
+
+  # 2. Style the nodes
+  geom_node_point(size = 7, fill = "#E69F00", color = "black", shape = 21) +
+
+  # 3. Add labels that automatically repel away from each other
+  geom_node_text(aes(label = name),
+                 repel = TRUE,
+                 size = 4,
+                 color = "#002B5B",
+                 bg.color = "white", # Adds a slight white halo for readability
+                 bg.r = 0.15) +
+
+  # 4. Clean up the background
+  theme_graph() +
+  theme(legend.position = "bottom")
+
